f29ba8aea8a9a048dd0151163e37332880796c6a
phuch <phuchmth2209055@fpt.edu.vn>
2024-06-05 09:13:27 +0700

    1 Commit

---
+++ src/main/java/com/example/iasftest/IasfTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.iasftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasfTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IasfTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iasftest/IasfTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iasftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasfTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
